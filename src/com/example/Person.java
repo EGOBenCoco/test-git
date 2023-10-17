b4fe9ad3458841b4fa271c111788eb664f2f0ae3
Kirill <test@gmail.com>
2023-10-17 12:39:50 +0300

    Adding class Person

---
+++ src/com/example/Person.java
@@ -0,0 +1,21 @@
+package com.example;
+
+public class Person extends BaseEntity
+{
+    public String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                '}';
+    }
+}
